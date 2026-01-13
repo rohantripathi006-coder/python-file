ff4ccda576c12b433e8f5e100ebed5bfb0a768d8
rohantripathi006-coder <rohantripathi006@gmail.com>
2026-01-13 10:16:11 +0530

    Add files via upload

---
+++ largensmall.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the first Number"))
+b=int(input("Enter the Second NUmber"))
+if(a>b):
+ print("A is greater and B is smaller")
+else:
+ print("B is greater and A is smaller")       
