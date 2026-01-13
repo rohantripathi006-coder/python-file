ae556e7d458956052ab3dae56a010f467c77436c
rohantripathi006-coder <rohantripathi006@gmail.com>
2026-01-13 10:34:09 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=int(input("Enter the third number:"))
+sum=a+b+c;
+if sum==180:
+ print("it is valid")
+else:
+ print("it is not valid")
